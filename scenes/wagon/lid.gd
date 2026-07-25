15c5f0c08062af0a73cf4cf02e6d629d752965ea
Nademtis <mathias.99.madsen@gmail.com>
2026-07-25 17:29:10 +0200

    oven lid is working

--- scenes/wagon/lid.gd
+++ scenes/wagon/lid.gd
@@ -1,12 +1,17 @@
 extends AnimatedSprite2D
 class_name Lid
 
+@onready var audio_lid_open_random: AudioStreamPlayer2D = $"../../Oven/AUDIO/audioLidOpenRandom"
+@onready var audio_lid_close_random: AudioStreamPlayer2D = $"../../Oven/AUDIO/audioLidCloseRandom"
+
+
 func open() -> void:
 		play("open")
+		audio_lid_open_random.play()
 	
 func close() -> void:
 		play("close")
-	
+		audio_lid_close_random.play()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
