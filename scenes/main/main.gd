857877624566e53701cf46bd3fa8ba8bffc48500
Nademtis <mathias.99.madsen@gmail.com>
2026-07-26 19:27:43 +0200

    5 levels

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -10,8 +10,12 @@ extends Node2D
 
 
 var level_list : Array[String] = [
-	#"res://scenes/levels/level_1.tscn", 
+	"res://scenes/levels/level_1.tscn", 
 	"res://scenes/levels/level_2.tscn", 
+	"res://scenes/levels/level_3.tscn", 
+	"res://scenes/levels/level_4.tscn", 
+	"res://scenes/levels/level_5.tscn", 
+	
 	
 	"res://scenes/levels/level_template.tscn",
 
